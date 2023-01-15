dc3fdaae3750795d590e9ae6d95d42fb850a8cc6
mohammadliazul <liazulislam0@gmail.com>
2023-01-15 22:04:20 +0600

    product details page frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import productData from './ProductData';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='app'>
+      {
+        productData.map(({_id, title, src, description, content, price, colors},index) => (
+          <div key={_id} className="details">
+            <div>
+              <img src={src[index]} alt="largeImg" className='large-img'/>
+            </div>
+
+            <div className='box'>
+              <div className='row'>
+                <h2>{title}</h2>
+                <span>{price}</span>
+              </div>
+              <div className='colors' >
+                {
+                  colors.map((color, index) => (
+                    <button key={index} style={{background: color}}></button>
+                  ))
+                }
+              </div>
+              <p>{description}</p>
+              <p>{content}</p>
+              <div className="thumb" >
+                {
+                  src.map((img, index) =>(
+                      <img src={img} alt="small img" key={index} 
+                      />
+                  ))
+                }
+              </div>
+              <button className='add-to-cart'>Add to cart</button>
+            </div>
+          </div>
+        ))
+      }
+    </section>
   );
 }
 
---
+++ src/ProductData.js
@@ -0,0 +1,22 @@
+import nike1 from './images/nike-1.png';
+import nike2 from './images/nike-2.png';
+import nike3 from './images/nike-3.png';
+import nike4 from './images/nike-4.png';
+
+const productData = [{
+  "_id": "1",
+  "title": "Nike Shoes",
+  "src": [
+    nike1,
+    nike2,
+    nike3,
+    nike4,
+  ],
+  "description": "UI/UX designing, html css tutorials",
+  "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+  "price": 23,
+  "colors":["red","black","crimson","teal"],
+  "count": 1
+}];
+
+export default productData;
